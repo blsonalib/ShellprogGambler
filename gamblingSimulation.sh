cf5cc23d30a23e85db1491b87b8aa579b00c9099
mugiwarapirates <hemilturakhia@gmail.com>
2019-11-13 17:03:57 +0530

    My Second Case

--- gamblingSimulation.sh
+++ gamblingSimulation.sh
@@ -2,22 +2,26 @@
 echo "welcome Gambler"
 read -p "Number of days to play:" numOfdays
 read -p "Number of games every day:" numOfgames
-bet=0
-stack=0
+
 for (( i=1 ; i<=$numOfdays ; i++ ))
 do
     stack=$(( $stack+100 ))
     for (( j=1; j <=$numOfgames ; j++ ))
     do
-       (( bet++ ))
-       if [ $stack -eq 0 ]
-       then
-       		break
-     	else
-      		(( stack-- ))
-     	fi
+         while (( $stack > 0 ))
+         do
+            bets=$(( RANDOM %2 ))
+            if [ $bets -eq 1 ]
+            then
+       		stack=$(( $satck+1 ))
+                echo "win"
+       	     else
+      		stack=$(( $stack-1 ))
+                echo "loose"
+     	     fi
+         done  
     done
-    	
 done
 echo $stack
-echo $bet
+
+
